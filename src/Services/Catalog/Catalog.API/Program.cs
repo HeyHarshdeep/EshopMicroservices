9c9aeaa743c85152f7eb1d8e1a404789d060e975
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-07-21 01:37:15 +0530

    Catalog.API added post endpoint and mediatR handlers

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the Container
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
+
+app.MapCarter();
 
 app.Run();
