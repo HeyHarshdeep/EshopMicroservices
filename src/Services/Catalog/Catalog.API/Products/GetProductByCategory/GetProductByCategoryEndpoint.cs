16199896e7226450d9a5719eaf50199fdbedff88
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-07-22 01:27:28 +0530

    Get Product By Category

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+//public record GetProductByCategoryRequest(String Category);
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string Category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(Category));
+
+            var response = result.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(result);
+        }).WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Category")
+        .WithDescription("Get Product By Category"); ;
+    }
+}
