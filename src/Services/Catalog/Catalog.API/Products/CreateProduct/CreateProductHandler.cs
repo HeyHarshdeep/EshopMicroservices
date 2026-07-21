75977de74768fd8d4585c314ce3dbf7d31a31596
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-07-22 00:37:05 +0530

    Get Product and By Id

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -7,7 +7,7 @@ namespace Catalog.API.Products.CreateProduct;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) :
     ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -22,10 +22,11 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
             Price = command.Price
         };
         //Save to the database
-
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         //Return CreateProductResult result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
 
 
     }
