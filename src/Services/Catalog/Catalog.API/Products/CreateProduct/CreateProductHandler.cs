a369cbb56c0404804710fed3415a0ee9cc490994
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-07-22 03:18:12 +0530

    Added Fluent validation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,6 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
+
 using System.Windows.Input;
 
 namespace Catalog.API.Products.CreateProduct;
@@ -7,10 +8,29 @@ namespace Catalog.API.Products.CreateProduct;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) :
     ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is Required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is Required");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is Required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Imagefile is Required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be Greater than 0");
+
+    }
+}
+internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+        if (errors.Any())
+        {
+            throw new ValidationException(errors.FirstOrDefault());
+        }
 
         //Create product entity from command object
         var product = new Product
