6acf1b80a708c7e13c1ee9213d4c360b2c6734d0
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-07-29 23:38:55 +0530

    Added Basket.API

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,36 @@
+using BuildingBlocks.Behavior;
+using BuildingBlocks.Exceptions.Handler;
+using JasperFx;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Service to the container
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(config =>
+{
+    config.Connection(builder.Configuration.GetConnectionString("Database")!);
+    config.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+//Configure the HTTP request and pipeline
+
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+app.Run();
