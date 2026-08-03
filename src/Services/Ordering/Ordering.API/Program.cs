b72935c31be9da944743bd16e8aa150064fa1945
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-08-03 21:13:42 +0530

    Setup Ordering.API

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,10 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add service to the container
 
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureService(builder.Configuration)
+    .AddApiServices();
+
 var app = builder.Build();
 
 //configure the http pipeline
-
+app.UseApiServices();
 
 app.Run();
