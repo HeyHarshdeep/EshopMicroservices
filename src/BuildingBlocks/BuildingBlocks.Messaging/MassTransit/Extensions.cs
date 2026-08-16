d2fff08cfbed64b5297728830aba40c8fef9042d
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-08-17 03:25:03 +0530

    Add BuildingBlocks.Messaging

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+    {
+        //Implement RabbitMQ MassTransit Configuration
+        return services;
+    }
+}
