624ff24cf8bbbb99c383ea0cab10634b2b4bcbc8
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-08-17 03:30:06 +0530

    Add MassTransit

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BuildingBlocks.Messaging.MassTransit;
-
-public static class Extensions
+public static class Extentions
 {
-    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+    public static IServiceCollection AddMessageBroker
+        (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
     {
-        //Implement RabbitMQ MassTransit Configuration
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]);
+                    host.Password(configuration["MessageBroker:Password"]);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
         return services;
     }
 }
