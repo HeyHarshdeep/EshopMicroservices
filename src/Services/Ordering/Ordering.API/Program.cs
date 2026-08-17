2a5050ed367ce8d75d362119b26ee63adc7d592c
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-08-18 02:00:36 +0530

    ordering api

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 //add service to the container
 
 builder.Services
-    .AddApplicationServices()
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureService(builder.Configuration)
     .AddApiServices(builder.Configuration);
 
